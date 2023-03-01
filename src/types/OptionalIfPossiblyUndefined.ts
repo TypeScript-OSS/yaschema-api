393135b97419439657f3481b9df5711bc69b045f
Brian Westphal <brian.westphal@panchaininc.com>
2023-03-02 06:13:51 +0800

    v2.0.1: - Corrected definition of OptionalIfPossiblyUndefined

--- src/types/OptionalIfPossiblyUndefined.ts
+++ src/types/OptionalIfPossiblyUndefined.ts
@@ -1,3 +1,5 @@
 export type OptionalIfPossiblyUndefined<K extends string | number | symbol, T> = [T] extends [undefined]
   ? Partial<Record<K, T>>
+  : [T] extends [T | undefined]
+  ? Partial<Record<K, T>>
   : Record<K, T>;
