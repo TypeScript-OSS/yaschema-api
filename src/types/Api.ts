dc7f8ac9db3beee6f6290df3272ccf11622c4e3a
Brian Westphal <brian.westphal@panchaininc.com>
2023-05-15 14:48:48 +0800

    v3.2.0: - All APIs now support `deprecated` field

--- src/types/Api.ts
+++ src/types/Api.ts
@@ -8,6 +8,10 @@ export interface Api {
   /** A name that can be used to describe this API, when logging for example */
   name: string;
 
+  /** If `true` or a `string`, use of this Api is deprecated and usage should be discontinued as soon as possible.  A string value may be
+   * used to document why the deprecation occurred and/or preferred upgrade strategies and APIs. */
+  deprecated?: boolean | string;
+
   /** A description, which can be used by code generation tools to generate documentation */
   description?: string;
 
