64f5d53543d5b86afe0cb534e8f1bfccf67588f2
Brian Westphal <brian.westphal@panchaininc.com>
2024-07-31 08:01:10 +0800

    v3.7.0: - `checkRequestValidation` and `checkResponseValidation` now include `validationErrorPath` when there's a validation error

--- src/utils/check-request-validation.ts
+++ src/utils/check-request-validation.ts
@@ -22,39 +22,63 @@ export const checkRequestValidation = ({
   validationMode: ValidationMode;
 }):
   | ({ ok: true } & (
-      | { hadSoftValidationError: false; invalidPart?: undefined; validationError?: undefined }
-      | { hadSoftValidationError: true; invalidPart: keyof GenericApiRequest; validationError: string }
+      | { hadSoftValidationError: false; invalidPart?: undefined; validationError?: undefined; validationErrorPath?: undefined }
+      | { hadSoftValidationError: true; invalidPart: keyof GenericApiRequest; validationError: string; validationErrorPath: string }
     ))
-  | { ok: false; invalidPart: keyof GenericApiRequest; validationError: string } => {
+  | { ok: false; invalidPart: keyof GenericApiRequest; validationError: string; validationErrorPath: string } => {
   if (reqHeaders.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'headers', validationError: reqHeaders.error };
+      return { ok: false, invalidPart: 'headers', validationError: reqHeaders.error, validationErrorPath: reqHeaders.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'headers', validationError: reqHeaders.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'headers',
+        validationError: reqHeaders.error,
+        validationErrorPath: reqHeaders.errorPath
+      };
     }
   }
 
   if (reqParams.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'params', validationError: reqParams.error };
+      return { ok: false, invalidPart: 'params', validationError: reqParams.error, validationErrorPath: reqParams.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'params', validationError: reqParams.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'params',
+        validationError: reqParams.error,
+        validationErrorPath: reqParams.errorPath
+      };
     }
   }
 
   if (reqQuery.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'query', validationError: reqQuery.error };
+      return { ok: false, invalidPart: 'query', validationError: reqQuery.error, validationErrorPath: reqQuery.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'query', validationError: reqQuery.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'query',
+        validationError: reqQuery.error,
+        validationErrorPath: reqQuery.errorPath
+      };
     }
   }
 
   if (reqBody.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'body', validationError: reqBody.error };
+      return { ok: false, invalidPart: 'body', validationError: reqBody.error, validationErrorPath: reqBody.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'body', validationError: reqBody.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'body',
+        validationError: reqBody.error,
+        validationErrorPath: reqBody.errorPath
+      };
     }
   }
 
--- src/utils/check-response-validation.ts
+++ src/utils/check-response-validation.ts
@@ -19,31 +19,49 @@ export const checkResponseValidation = ({
   validationMode: ValidationMode;
 }):
   | ({ ok: true } & (
-      | { hadSoftValidationError: false; invalidPart?: undefined; validationError?: undefined }
-      | { hadSoftValidationError: true; invalidPart: keyof GenericApiResponse; validationError: string }
+      | { hadSoftValidationError: false; invalidPart?: undefined; validationError?: undefined; validationErrorPath?: undefined }
+      | { hadSoftValidationError: true; invalidPart: keyof GenericApiResponse; validationError: string; validationErrorPath: string }
     ))
-  | { ok: false; invalidPart: keyof GenericApiResponse; validationError: string } => {
+  | { ok: false; invalidPart: keyof GenericApiResponse; validationError: string; validationErrorPath: string } => {
   if (resStatus.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'status', validationError: resStatus.error };
+      return { ok: false, invalidPart: 'status', validationError: resStatus.error, validationErrorPath: resStatus.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'status', validationError: resStatus.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'status',
+        validationError: resStatus.error,
+        validationErrorPath: resStatus.errorPath
+      };
     }
   }
 
   if (resHeaders.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'headers', validationError: resHeaders.error };
+      return { ok: false, invalidPart: 'headers', validationError: resHeaders.error, validationErrorPath: resHeaders.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'headers', validationError: resHeaders.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'headers',
+        validationError: resHeaders.error,
+        validationErrorPath: resHeaders.errorPath
+      };
     }
   }
 
   if (resBody.error !== undefined) {
     if (validationMode === 'hard') {
-      return { ok: false, invalidPart: 'body', validationError: resBody.error };
+      return { ok: false, invalidPart: 'body', validationError: resBody.error, validationErrorPath: resBody.errorPath };
     } else {
-      return { ok: true, hadSoftValidationError: true, invalidPart: 'body', validationError: resBody.error };
+      return {
+        ok: true,
+        hadSoftValidationError: true,
+        invalidPart: 'body',
+        validationError: resBody.error,
+        validationErrorPath: resBody.errorPath
+      };
     }
   }
 
